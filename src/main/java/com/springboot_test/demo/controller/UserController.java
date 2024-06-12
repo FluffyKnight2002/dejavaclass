832d63dfbb11f663a2334a9ca72ed18e47d20f19
CZe <aungkhentzaw@gmail.com>
2024-06-12 21:35:14 +0630

    day 2

--- src/main/java/com/springboot_test/demo/controller/UserController.java
+++ src/main/java/com/springboot_test/demo/controller/UserController.java
@@ -4,9 +4,14 @@ import com.springboot_test.demo.model.User;
 import com.springboot_test.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,12 +21,12 @@ public class UserController {
 
 
     @GetMapping("/home")
-    public String home(){
+    public String home() {
         return "home";
     }
 
     @PostMapping("/create")
-    public String create(@RequestParam("username") String username, @RequestParam("email") String email){
+    public String create(@RequestParam("username") String username, @RequestParam("email") String email) {
         User user = new User();
         user.setUsername(username);
         user.setEmail(email);
@@ -31,5 +36,39 @@ public class UserController {
         return "redirect:/home";
     }
 
+    @GetMapping("/findAll")
+    public String findAll(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
+        return "list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userRepository.deleteById(id);
+        return "redirect:/findAll";
+    }
+
+    @GetMapping("/update/{id}")
+    public String edit(@PathVariable("id") int id, Model model) {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()) {
+            model.addAttribute("user", user.get());
+            return "edit";
+        }
+        return "redirect:/findAll";
+    }
+
+    @PostMapping("/update")
+    public String update(@RequestParam("id") int id, @RequestParam("username") String username, @RequestParam("email") String email) {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()){
+            user.get().setUsername(username);
+            user.get().setEmail(email);
+            userRepository.save(user.get());
+            return "redirect:/findAll";
+        }
+        return "redirect:/findAll";
+    }
 
 }
